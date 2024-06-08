ef58e6875e380e30d9b63227c3a154d1084976ab
KMG5498 <kt7447@kaist.ac.kr>
2024-06-08 11:30:43 +0900

    Update odenet_mnist.py

--- examples/odenet_mnist.py
+++ examples/odenet_mnist.py
@@ -5,9 +5,11 @@ import time
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from torchdiffeq import odeint_adjoint as odeint
 
 # Argument parsing
 parser = argparse.ArgumentParser()
@@ -20,200 +22,13 @@ parser.add_argument('--data_aug', type=eval, default=True, choices=[True, False]
 parser.add_argument('--lr', type=float, default=0.1)
 parser.add_argument('--batch_size', type=int, default=128)
 parser.add_argument('--test_batch_size', type=int, default=1000)
+parser.add_argument('--dropout', type=float, default=0.5)
 parser.add_argument('--save', type=str, default='./experiment1')
 parser.add_argument('--debug', action='store_true')
 parser.add_argument('--gpu', type=int, default=0)
 args = parser.parse_args()
 
-if args.adjoint:
-    from torchdiffeq import odeint_adjoint as odeint
-else:
-    from torchdiffeq import odeint
-
-# Define functions and classes
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-def norm(dim):
-    return nn.GroupNorm(min(32, dim), dim)
-
-class ResBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(ResBlock, self).__init__()
-        self.norm1 = norm(inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.norm2 = norm(planes)
-        self.conv2 = conv3x3(planes, planes)
-
-    def forward(self, x):
-        shortcut = x
-        out = self.relu(self.norm1(x))
-        if self.downsample is not None:
-            shortcut = self.downsample(out)
-        out = self.conv1(out)
-        out = self.norm2(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        return out + shortcut
-
-class ConcatConv2d(nn.Module):
-    def __init__(self, dim_in, dim_out, ksize=3, stride=1, padding=0, dilation=1, groups=1, bias=True, transpose=False):
-        super(ConcatConv2d, self).__init__()
-        module = nn.ConvTranspose2d if transpose else nn.Conv2d
-        self._layer = module(dim_in + 1, dim_out, kernel_size=ksize, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
-
-    def forward(self, t, x):
-        tt = torch.ones_like(x[:, :1, :, :]) * t
-        ttx = torch.cat([tt, x], 1)
-        return self._layer(ttx)
-
-class ODEfunc(nn.Module):
-    def __init__(self, dim):
-        super(ODEfunc, self).__init__()
-        self.norm1 = norm(dim)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv1 = ConcatConv2d(dim, dim, 3, 1, 1)
-        self.norm2 = norm(dim)
-        self.conv2 = ConcatConv2d(dim, dim, 3, 1, 1)
-        self.norm3 = norm(dim)
-        self.nfe = 0
-
-    def forward(self, t, x):
-        self.nfe += 1
-        out = self.norm1(x)
-        out = self.relu(out)
-        out = self.conv1(t, out)
-        out = self.norm2(out)
-        out = self.relu(out)
-        out = self.conv2(t, out)
-        out = self.norm3(out)
-        return out
-
-class ODEBlock(nn.Module):
-    def __init__(self, odefunc):
-        super(ODEBlock, self).__init__()
-        self.odefunc = odefunc
-        self.integration_time = torch.tensor([0, 1]).float()
-
-    def forward(self, x):
-        self.integration_time = self.integration_time.type_as(x)
-        out = odeint(self.odefunc, x, self.integration_time, method='rk4', rtol=args.tol, atol=args.tol)
-        return out[1]
-
-    @property
-    def nfe(self):
-        return self.odefunc.nfe
-
-    @nfe.setter
-    def nfe(self, value):
-        self.odefunc.nfe = value
-
-class Flatten(nn.Module):
-    def __init__(self):
-        super(Flatten, self).__init__()
-
-    def forward(self, x):
-        shape = torch.prod(torch.tensor(x.shape[1:])).item()
-        return x.view(-1, shape)
-
-class RunningAverageMeter(object):
-    def __init__(self, momentum=0.99):
-        self.momentum = momentum
-        self.reset()
-
-    def reset(self):
-        self.val = None
-        self.avg = 0
-
-    def update(self, val):
-        if self.val is None:
-            self.avg = val
-        else:
-            self.avg = self.avg * self.momentum + val * (1 - self.momentum)
-        self.val = val
-
-def get_cifar10_loaders(data_aug=False, batch_size=128, test_batch_size=1000, perc=1.0):
-    if data_aug:
-        transform_train = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-        ])
-    else:
-        transform_train = transforms.Compose([
-            transforms.ToTensor(),
-        ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-
-    train_loader = DataLoader(
-        datasets.CIFAR10(root='.data/cifar10', train=True, download=True, transform=transform_train),
-        batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True
-    )
-
-    train_eval_loader = DataLoader(
-        datasets.CIFAR10(root='.data/cifar10', train=True, download=True, transform=transform_test),
-        batch_size=test_batch_size, shuffle=False, num_workers=2, drop_last=True
-    )
-
-    test_loader = DataLoader(
-        datasets.CIFAR10(root='.data/cifar10', train=False, download=True, transform=transform_test),
-        batch_size=test_batch_size, shuffle=False, num_workers=2, drop_last=True
-    )
-
-    return train_loader, test_loader, train_eval_loader
-
-def inf_generator(iterable):
-    iterator = iterable.__iter__()
-    while True:
-        try:
-            yield iterator.__next__()
-        except StopIteration:
-            iterator = iterable.__iter__()
-
-def learning_rate_with_decay(batch_size, batch_denom, batches_per_epoch, boundary_epochs, decay_rates):
-    initial_learning_rate = args.lr * batch_size / batch_denom
-    boundaries = [int(batches_per_epoch * epoch) for epoch in boundary_epochs]
-    vals = [initial_learning_rate * decay for decay in decay_rates]
-
-    def learning_rate_fn(itr):
-        lt = [itr < b for b in boundaries] + [True]
-        i = np.argmax(lt)
-        return vals[i]
-
-    return learning_rate_fn
-
-def one_hot(x, K):
-    return np.array(x[:, None] == np.arange(K)[None, :], dtype=int)
-
-def accuracy(model, dataset_loader):
-    total_correct = 0
-    for x, y in dataset_loader:
-        x = x.to(device)
-        y = one_hot(np.array(y.numpy()), 10)
-        target_class = np.argmax(y, axis=1)
-        predicted_class = np.argmax(model(x).cpu().detach().numpy(), axis=1)
-        total_correct += np.sum(predicted_class == target_class)
-    return total_correct / len(dataset_loader.dataset)
-
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-def makedirs(dirname):
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-
+# Logging setup
 def get_logger(logpath, filepath, package_files=[], displaying=True, saving=True, debug=False):
     logger = logging.getLogger()
     if debug:
@@ -236,82 +51,268 @@ def get_logger(logpath, filepath, package_files=[], displaying=True, saving=True
             logger.info(package_f.read())
     return logger
 
-# Set up logging
-makedirs(args.save)
-logger = get_logger(logpath=os.path.join(args.save, 'logs'), filepath=os.path.abspath(__file__), package_files=[os.path.abspath(__file__)], displaying=not args.debug)
+# Define normalization layer (can be BatchNorm or GroupNorm)
+def norm(dim):
+    return nn.BatchNorm2d(dim)
 
-device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
+# 3x3 convolution with padding
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
 
-# Set random seed
-torch.manual_seed(0)
-np.random.seed(0)
+# 1x1 convolution
+def conv1x1(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
-# Load data
-train_loader, test_loader, train_eval_loader = get_cifar10_loaders(args.data_aug, args.batch_size, args.test_batch_size)
+# Basic Residual Block
+class ResBlock(nn.Module):
+    expansion = 1
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(ResBlock, self).__init__()
+        self.norm1 = norm(inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.norm2 = norm(planes)
+        self.conv2 = conv3x3(planes, planes)
 
-# Build model
-downsampling_layers = [
-    nn.Conv2d(3, 64, 3, 1, padding=1),  # Input channels changed to 3 for CIFAR-10
-    norm(64),
-    nn.ReLU(inplace=True),
-    nn.Conv2d(64, 64, 3, 1, padding=1),
-    norm(64),
-    nn.ReLU(inplace=True),
-    nn.Conv2d(64, 64, 3, 1, padding=1),
-]
+    def forward(self, x):
+        shortcut = x
+        out = self.relu(self.norm1(x))
+        if self.downsample is not None:
+            shortcut = self.downsample(out)
+        out = self.conv1(out)
+        out = self.norm2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        return out + shortcut
 
-feature_layers = [ODEBlock(ODEfunc(64))] if args.network == 'odenet' else [ResBlock(64, 64) for _ in range(6)]
+# ODE Block
+class ODEBlock(nn.Module):
+    def __init__(self, odefunc):
+        super(ODEBlock, self).__init__()
+        self.odefunc = odefunc
+        self.integration_time = torch.tensor([0, 1]).float()
 
-fc_layers = [
-    norm(64),
-    nn.ReLU(inplace=True),
-    nn.AdaptiveAvgPool2d((1, 1)),
-    Flatten(),
-    nn.Linear(64, 10)  # CIFAR-10 has 10 classes
-]
+    def forward(self, x):
+        self.integration_time = self.integration_time.type_as(x)
+        out = odeint(self.odefunc, x, self.integration_time, method='rk4', rtol=args.tol, atol=args.tol)
+        return out[1]
 
-model = nn.Sequential(*downsampling_layers, *feature_layers, *fc_layers).to(device)
-logger.info(model)
-logger.info('Number of parameters: {}'.format(count_parameters(model)))
+# ODE Function
+class ODEfunc(nn.Module):
+    def __init__(self, dim):
+        super(ODEfunc, self).__init__()
+        self.norm1 = norm(dim)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = ConcatConv2d(dim, dim, 3, 1, 1)
+        self.norm2 = norm(dim)
+        self.conv2 = ConcatConv2d(dim, dim, 3, 1, 1)
+        self.norm3 = norm(dim)
+        self.nfe = 0
 
-# Training
-criterion = nn.CrossEntropyLoss().to(device)
-optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, nesterov=True, weight_decay=5e-4)
+    def forward(self, t, x):
+        self.nfe += 1
+        out = self.norm1(x)
+        out = self.relu(out)
+        out = self.conv1(t, out)
+        out = self.norm2(out)
+        out = self.relu(out)
+        out = self.conv2(t, out)
+        out = self.norm3(out)
+        return out
 
-if args.network == 'odenet':
-    lr_fn = learning_rate_with_decay(args.batch_size, batch_denom=128, batches_per_epoch=len(train_loader), boundary_epochs=[60, 80, 100], decay_rates=[1, 0.2, 0.04, 0.008])
-else:
-    lr_fn = learning_rate_with_decay(args.batch_size, batch_denom=128, batches_per_epoch=len(train_loader), boundary_epochs=[80, 120, 160], decay_rates=[1, 0.1, 0.01])
+# Concatenated Convolution
+class ConcatConv2d(nn.Module):
+    def __init__(self, dim_in, dim_out, ksize=3, stride=1, padding=0, dilation=1, groups=1, bias=True, transpose=False):
+        super(ConcatConv2d, self).__init__()
+        module = nn.ConvTranspose2d if transpose else nn.Conv2d
+        self._layer = module(
+            dim_in + 1, dim_out, kernel_size=ksize, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias
+        )
 
-best_acc = 0
-for epoch in range(args.nepochs):
-    model.train()
-    start = time.time()
-    for itr, (x, y) in enumerate(train_loader):
+    def forward(self, t, x):
+        tt = torch.ones_like(x[:, :1, :, :]) * t
+        ttx = torch.cat([tt, x], 1)
+        return self._layer(ttx)
+
+# Flatten Layer
+class Flatten(nn.Module):
+    def forward(self, x):
+        shape = torch.prod(torch.tensor(x.shape[1:])).item()
+        return x.view(-1, shape)
+
+# Running Average Meter
+class RunningAverageMeter(object):
+    def __init__(self, momentum=0.99):
+        self.momentum = momentum
+        self.reset()
+
+    def reset(self):
+        self.val = None
+        self.avg = 0
+
+    def update(self, val):
+        if self.val is None:
+            self.avg = val
+        else:
+            self.avg = self.avg * self.momentum + val * (1 - self.momentum)
+        self.val = val
+
+# Get CIFAR-10 Data Loaders
+def get_cifar10_loaders(data_aug=True, batch_size=128, test_batch_size=1000):
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    test_loader = DataLoader(testset, batch_size=test_batch_size, shuffle=False, num_workers=2)
+
+    return train_loader, test_loader
+
+# Learning Rate Scheduling
+def learning_rate_with_decay(batch_size, batch_denom, batches_per_epoch, boundary_epochs, decay_rates):
+    initial_learning_rate = args.lr * batch_size / batch_denom
+    boundaries = [int(batches_per_epoch * epoch) for epoch in boundary_epochs]
+    vals = [initial_learning_rate * decay for decay in decay_rates]
+
+    def learning_rate_fn(itr):
+        lt = [itr < b for b in boundaries] + [True]
+        i = np.argmax(lt)
+        return vals[i]
+
+    return learning_rate_fn
+
+# Accuracy Calculation
+def accuracy(model, dataset_loader):
+    total_correct = 0
+    total_samples = 0
+    model.eval()
+    with torch.no_grad():
+        for x, y in dataset_loader:
+            x, y = x.to(device), y.to(device)
+            logits = model(x)
+            _, predicted = torch.max(logits, 1)
+            total_correct += (predicted == y).sum().item()
+            total_samples += y.size(0)
+    return total_correct / total_samples
+
+# Model Definition
+class CIFAR10Net(nn.Module):
+    def __init__(self, is_odenet):
+        super(CIFAR10Net, self).__init__()
+        self.is_odenet = is_odenet
+
+        if args.downsampling_method == 'conv':
+            downsampling_layers = [
+                nn.Conv2d(                3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                norm(64),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                norm(64),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                norm(64),
+                nn.ReLU(inplace=True),
+            ]
+        elif args.downsampling_method == 'res':
+            downsampling_layers = [
+                nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                ResBlock(64, 64, stride=1),
+                ResBlock(64, 64, stride=1),
+                nn.ReLU(inplace=True),
+            ]
+
+        feature_layers = [ODEBlock(ODEfunc(64))] if is_odenet else [
+            ResBlock(64, 64) for _ in range(3)
+        ]
+
+        fc_layers = [
+            norm(64),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            Flatten(),
+            nn.Dropout(args.dropout),
+            nn.Linear(64, 10)
+        ]
+
+        self.model = nn.Sequential(
+            *downsampling_layers,
+            *feature_layers,
+            *fc_layers
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+# Main function
+if __name__ == '__main__':
+    makedirs(args.save)
+    logger = get_logger(logpath=os.path.join(args.save, 'logs'), filepath=os.path.abspath(__file__))
+    logger.info(args)
+
+    device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
+
+    is_odenet = args.network == 'odenet'
+
+    train_loader, test_loader = get_cifar10_loaders(args.data_aug, args.batch_size, args.test_batch_size)
+    data_gen = inf_generator(train_loader)
+    batches_per_epoch = len(train_loader)
+
+    lr_fn = learning_rate_with_decay(
+        args.batch_size, batch_denom=128, batches_per_epoch=batches_per_epoch, boundary_epochs=[100, 150],
+        decay_rates=[1, 0.1, 0.01]
+    )
+
+    model = CIFAR10Net(is_odenet).to(device)
+    logger.info(model)
+    logger.info('Number of parameters: {}'.format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
+
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+
+    best_acc = 0
+    batch_time_meter = RunningAverageMeter()
+    end = time.time()
+
+    for itr in range(args.nepochs * batches_per_epoch):
+        model.train()
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr_fn(itr)
+
         optimizer.zero_grad()
-        x = x.to(device)
-        y = y.to(device)
+        x, y = data_gen.__next__()
+        x, y = x.to(device), y.to(device)
         logits = model(x)
         loss = criterion(logits, y)
         loss.backward()
         optimizer.step()
 
-        if itr % 50 == 0 or itr == len(train_loader) - 1:
-            logger.info('Iter {:04d} | Epoch {:03d} | Loss {:.6f}'.format(itr, epoch, loss.item()))
+        batch_time_meter.update(time.time() - end)
+        end = time.time()
 
-    model.eval()
-    with torch.no_grad():
-        train_acc = accuracy(model, train_eval_loader)
-        val_acc = accuracy(model, test_loader)
-        logger.info('Epoch {:03d} | Time {:.2f} | Train Acc {:.4f} | Val Acc {:.4f}'.format(epoch, time.time() - start, train_acc, val_acc))
+        if itr % batches_per_epoch == 0:
+            model.eval()
+            train_acc = accuracy(model, train_loader)
+            test_acc = accuracy(model, test_loader)
+            if test_acc > best_acc:
+                torch.save({'state_dict': model.state_dict(), 'args': args}, os.path.join(args.save, 'model.pth'))
+                best_acc = test_acc
 
-        if val_acc > best_acc:
-            best_acc = val_acc
-            torch.save({'state_dict': model.state_dict(), 'args': args}, os.path.join(args.save, 'model.pth'))
-
-# Final test accuracy calculation and logging
-model.eval()
-test_acc = accuracy(model, test_loader)
-logger.info('Final Test Accuracy: {:.4f}'.format(test_acc))
-logger.info('Training complete. Best validation accuracy: {:.4f}'.format(best_acc))
+            logger.info(
+                "Epoch {:04d}/{:04d} | Batch {:04d}/{:04d} | Time {:.3f} ({:.3f}) | Train Acc {:.4f} | Test Acc {:.4f}".format(
+                    itr // batches_per_epoch, args.nepochs, itr % batches_per_epoch, batches_per_epoch,
+                    batch_time_meter.val, batch_time_meter.avg, train_acc, test_acc
+                )
+            )
 
