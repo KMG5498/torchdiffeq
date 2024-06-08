215be64d91def0bd9619a7ac7cd51476d288fead
KMG5498 <kt7447@kaist.ac.kr>
2024-06-08 10:40:08 +0900

    Update odenet_mnist.py
    전체

--- examples/odenet_mnist.py
+++ examples/odenet_mnist.py
@@ -234,24 +234,7 @@ def get_logger(logpath, filepath, package_files=[], displaying=True, saving=True
         logger.info(f)
         with open(f, "r") as package_f:
             logger.info(package_f.read())
-
     return logger
-
-class RunningAverageMeter(object):
-    def __init__(self, momentum=0.99):
-        self.momentum = momentum
-        self.reset()
-
-    def reset(self):
-        self.val = None
-        self.avg = 0
-
-    def update(self, val):
-        if self.val is None:
-            self.avg = val
-        else:
-            self.avg = self.avg * self.momentum + val * (1 - self.momentum)
-        self.val = val
 
 # Set up logging
 makedirs(args.save)
